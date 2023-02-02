e5f691d92fe5be6c341bfb358547eab3e8c823fb
Janic Weber <janic.weber@gmail.com>
2023-02-02 10:44:40 +0100

    remove class import

--- src/adaptsim/__init__.py
+++ src/adaptsim/__init__.py
@@ -4,7 +4,6 @@ this folder holds the adaptsim package
 Created on Wed Jan 27 12:28:31 2023
 @author: janicweber
 """
-from .afs import MC_object
 from .constants import ALL_SIM_DICT, KEY_DICT_SIM
 
 __all__ = ['']
