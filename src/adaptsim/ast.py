666f587fe5f516728cae3941c311744442195f07
Janic Weber <janic.weber@uzh.ch>
2023-02-05 15:36:29 +0100

    only read in one file

--- src/adaptsim/ast.py
+++ src/adaptsim/ast.py
@@ -13,38 +13,38 @@ class MC_object():
     of calculation, invoke keys and define
     calculation settings from file
     """
-    def __init__(self, model_filename, simulation_filename):
+    def __init__(self, model_filename):
         model = afx.RL_object(model_filename)
 
         try: # check if file can be opened
-            with open(simulation_filename, 'r') as f:
+            with open(model_filename, 'r') as f:
                 read_in = f.read()
             raw_simulation_dict = json.loads(read_in)
         except TypeError:
-            if isinstance(simulation_filename, dict):
-                raw_simulation_dict = simulation_filename
+            if isinstance(model_filename, dict):
+                raw_simulation_dict = model_filename
             else:
-                afx.aft_error(f'"{simulation_filename}" not a filename or dict', nme)
+                afx.aft_error(f'"{model_filename}" not a filename or dict', nme)
         except SyntaxError as syntax_err:
-            afx.aft_error(f'error in "{simulation_filename}", {syntax_err}', nme)
+            afx.aft_error(f'error in "{model_filename}", {syntax_err}', nme)
         except OSError:
-            afx.aft_error(f'No such file: "{simulation_filename}"', nme)
+            afx.aft_error(f'No such file: "{model_filename}"', nme)
         except ValueError as decode_err:
-            afx.aft_error(f'decode error in "{simulation_filename}", {decode_err}', nme)
+            afx.aft_error(f'decode error in "{model_filename}", {decode_err}', nme)
         except:
-            afx.aft_error(f'error in "{simulation_filename}", {sys.exc_info()}', nme)
+            afx.aft_error(f'error in "{model_filename}", {sys.exc_info()}', nme)
 
         try:
-            algorithm_simulation = raw_simulation_dict['algorithm']
+            algorithm_simulation = raw_simulation_dict['algorithm_simulation']
         except KeyError as algo_err:
-            afx.aft_error(f'{algo_err} key missing in: "{simulation_filename}"', nme)
+            afx.aft_error(f'{algo_err} key missing in: "{model_filename}"', nme)
         else:
             afx.aft_message_info('algorithm simulation:', algorithm_simulation, nme, 1)
 
         try: # check if simulation_keys exists and is a dictionnary
-            raw_keys = raw_simulation_dict['simulation_keys']
+            raw_keys = raw_simulation_dict['keys_simulation']
         except KeyError:
-            afx.aft_error(f'"simulation_keys" is missing in : "{simulation_filename}"', nme)
+            afx.aft_error(f'"keys_simulation" is missing in : "{model_filename}"', nme)
         else:
             simulation_dict = afx.key_reader(afs.KEY_DICT_SIM, afs.ALL_SIM_DICT, raw_keys, 'sim')
             afx.aft_message_dict('simulation', simulation_dict, nme)
@@ -56,11 +56,14 @@ class MC_object():
         self.settings = model.settings
         self.algorithm_simulation = algorithm_simulation
         self.keys_simulation = afx.DotDict(simulation_dict)
-        self.simulation_filename = simulation_filename
+        self.simulation_filename = model_filename
 
 
     def simulate(self):
-        use_tex = self.keys_simulation.use_tex
+        plot_sets = afs. RCPARAMS
+        plot_sets["text.usetex"] = self.keys_simulation.use_tex
+        plot_sets["figure.figsize"] = self.keys_simulation.figsize
+        plot_sets["font.size"] = self.keys_simulation.fontsize
         n_frac = self.keys_model.number_of_fractions
         if self.algorithm_simulation == 'histogram':
             n_patients = self.keys_simulation.n_patients 
@@ -73,9 +76,9 @@ class MC_object():
                 # output.oar_sum output.tumor_sum
                 n_frac_used = np.count_nonzero(~np.isnan(output.physical_doses))
                 plans[i] = n_frac_used
-            hist = afs.plot_hist(plans, n_frac, use_tex)
+            hist = afs.plot_hist(plans, n_frac, plot_sets)
 
-            if self.keys_simulation.plot_bool:
+            if self.keys_simulation.save:
                 afs.save_plot(hist, self.simulation_filename)
             
         elif self.algorithm_simulation == 'fraction':
@@ -87,9 +90,9 @@ class MC_object():
                 output = afx.multiple(self.algorithm, self.keys_model, self.settings)
                 c_dose_array[i] = output.tumor_doses
             self.c_dose_list = c_dose_array
-            fracs = afs.plot_dose(self.c_dose_list, sf_list, n_frac, c_list, use_tex)
+            fracs = afs.plot_dose(self.c_dose_list, sf_list, n_frac, c_list, plot_sets)
 
-            if self.keys_simulation.plot_bool:
+            if self.keys_simulation.save:
                 afs.save_plot(fracs, self.simulation_filename)
         
     def plot(self):
@@ -105,21 +108,14 @@ def main():
     )
     parser.add_argument(
         '-f',
-        '--fractionation',
-        metavar='',
-        help='input adaptive fractionation instruction filename',
-        type=str
-    )
-    parser.add_argument(
-        '-s',
-        '--simulation',
+        '--filename',
         metavar='',
         help='input adaptive fractionation instruction filename',
         type=str
     )
     # In case there is no input show help
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
-    sim = MC_object(args.fractionation, args.simulation)
+    sim = MC_object(args.filename)
 
     afx.aft_message('start session...', nme, 1)
     sim.simulate()
